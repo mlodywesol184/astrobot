245d524559e8f7cbecf41fcf5666bc0c0849406a
mlodywesol184 <44980730+mlodywesol184@users.noreply.github.com>
2019-01-03 22:29:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,8 @@ client.on('ready', () =>
   console.log('Bot is ready!');
 });
 
+//---------------------------------autoranga, powitanie----------------------------
+
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find(ch => ch.name === 'cześć');
   if (!channel) return;
@@ -17,123 +19,23 @@ client.on('guildMemberAdd', member => {
   member.addRole(czlonek.id)
 });
 
+//---------------------------------komendy------------------------------------
 
 client.on("message", message => {
 if(message.author.bot) return;
 if(message.content.startsWith(prefix))
 {
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
   const swojgosc = message.guild.roles.find(role => role.name ==="Swój Gość");
   const admin = message.guild.roles.find(role => role.name === "*administrator*");
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
-  if(command === "help")
-  {
-    if(!message.member.roles.has(swojgosc.id)) return message.reply("Tylko użytkownicy posiadający rangę Swój gość mogą używać bota. Więcej informacji pod komendą !swojgosc")
-    {
-      const kanal = message.guild.channels.find(ch =>  ch.name === "uzyskaj-rangę")
-      message.channel.send(`Pamiętaj, aby korzystać z bota, musisz posiadać rangę Swój Gość (info pod komendą !swojgosc)\nKomendy dla wszystkich:\n!help - wyświetla listę komend oraz ich przeznaczenie\n!facebook - wyświetla link do facebooka GWD\n!grupa - wyświetla link do grupy Astronomia-Polska\n!meteo - wyświetla link do grupy meteorologia-polska\n!stellarium - link do programu stellarium\n!swiatlo - link do lightpollutionmap\n!swojgosc(dostępna dla nieposiadających tej rangi) - informacje na temat rangi Swój Gość\n!randomuser - wybiera losowego użytkownika serwera\n!zjawisko - wyświetla dzisiejsze zjawisko astronomiczne\n!zjawisko [dzień/miesiac/rok] - wyświetla zjawisko na dany dzień\n!ranga [link do Twojego facebooka] - dostępna tylko na kanale ${kanal}, użyj jej jeśli chcesz uzyskać rangę Swój Gość\n\nKomendy dla administratorów:\n!clearchannel [liczba] - usuwa daną ilość wiadomości z kanału\n!ban [oznacz użytkownika] - chyba nie trzeba wyjaśniać\n!kick [oznacz użytkownka] - wyrzuca (nie banuje) danego użytkownika)
-      `)
-    }
-  }
-
-
-  if(command === "facebook")
-  {
-    if(!message.member.roles.has(swojgosc.id)) return message.reply("Tylko użytkownicy posiadający rangę Swój gość mogą używać bota. Więcej informacji pod komendą !swojgosc")
-    {
-      message.channel.send("Facebook Gwiazdy w Dłoniach: https://www.facebook.com/gwiazdywdloniach");
-    }
-  }
-
-  if(command === "grupa")
-  {
-    if(!message.member.roles.has(swojgosc.id)) return message.reply("Tylko użytkownicy posiadający rangę Swój gość mogą używać bota. Więcej informacji pod komendą !swojgosc")
-    {
-      message.channel.send("Wpadnij na grupę Astronomia-Polska: https://www.facebook.com/groups/astronomiapolska");
-    }
-  }
-
-  if(command === "meteo")
-  {
-    if(!message.member.roles.has(swojgosc.id)) return message.reply("Tylko użytkownicy posiadający rangę Swój gość mogą używać bota. Więcej informacji pod komendą !swojgosc")
-    {
-      message.channel.send("Grupa Meteorologia-Polska: https://www.facebook.com/groups/meteopolska");
-    }
-  }
-
-  if(command === "stellarium")
-  {
-    if(!message.member.roles.has(swojgosc.id)) return message.reply("Tylko użytkownicy posiadający rangę Swój gość mogą używać bota. Więcej informacji pod komendą !swojgosc")
-    {
-      message.channel.send("Interaktywna mapa nieba: https://stellarium.org");
-    }
-  }
-
-  if(command === "swiatlo")
-  {
-    if(!message.member.roles.has(swojgosc.id)) return message.reply("Tylko użytkownicy posiadający rangę Swój gość mogą używać bota. Więcej informacji pod komendą !swojgosc")
-    {
-      message.channel.send("Mapa zanieczyszczenia światłem: https://www.lightpollutionmap.info/");
-    }
-  }
+//-------------------------------dla nowych------------------------------------
 
   if(command === "swojgosc")
   {
       const kanal = message.guild.channels.find(ch => ch.name === "uzyskaj-rangę")
       message.channel.send(`Ranga Swój Gość oznacza, że jesteś zweryfikowanym użytkownikiem naszego discorda. Umożliwi Ci zmianę własnego pseudonimu, umieszczanie linków oraz załączanie plików, a także uczestnictwo w kanłach głosowych oraz korzystanie z bota.\nAby ją uzyskać, użyj komendy !ranga [link do Twojego facebooka] na kanale ${kanal}`);
-  }
-
-  if(command === "randomuser")
-  {
-    if(!message.member.roles.has(swojgosc.id)) return message.reply("Tylko użytkownicy posiadający rangę Swój gość mogą używać bota. Więcej informacji pod komendą !swojgosc")
-    {
-      const member = message.guild.members.random()
-      message.channel.send(`Wybieram ${member}!`);
-    }
-  }
-
-
- if(command === "zjawisko" || command==="zjawiska")
-  {
-    let data = args[0]
-    if (!data)
-    {
-     if(!message.member.roles.has(swojgosc.id)) return message.reply("Tylko użytkownicy posiadający rangę Swój gość mogą używać bota. Więcej informacji pod komendą !swojgosc")
-     {
-      const lista = require("./zjawiska.json");
-      let dzisiaj = new Date();
-      let dzien = dzisiaj.getDate();
-      let miesiac = dzisiaj.getMonth()+1;
-      let rok = dzisiaj.getFullYear();
-      dzisiaj = dzien + "/" + miesiac + "/" + rok;
-      let entry = lista.find(post => post.data === dzisiaj)
-      if(!entry) return message.channel.send("Brak zjawisk na dziś :(");
-      message.channel.send(`Dziś na niebie: ${entry.nazwa}`)
-     }
-    }
-  else
-  {
-    if(!message.member.roles.has(swojgosc.id)) return message.reply("Tylko użytkownicy posiadający rangę Swój gość mogą używać bota. Więcej informacji pod komendą !swojgosc")
-     {
-       const plik = require("./zjawiska.json");
-       let entry = plik.find(post => post.data === data);
-       if(!entry) return message.channel.send("Brak zjawisk na ten dzień");
-       message.channel.send(`Dnia ${data} nastąpi: ${entry.nazwa}`)
-     }
-  }
-  }
-
-
-
-  if(command === "clearchannel")
-  {
-    if(!message.member.roles.has(admin.id)) return message.reply("Ta komenda jest dostępna tylko dla administratorów")
-    {
-      let ilosc = args[0];
-      if(!ilosc) return message.channel.send("Podaj ilość wiadomości do usunięcia")
-      message.channel.bulkDelete(ilosc);
-    }
   }
 
   if(command === "ranga")
@@ -170,20 +72,135 @@ if(message.content.startsWith(prefix))
 	  }
   }
 
+//--------------------------------zwykłe--------------------------------------
+
+if(message.member.roles.has(swojgosc.id))
+ {
+
+  if(command === "help")
+  {
+      const kanal = message.guild.channels.find(ch =>  ch.name === "uzyskaj-rangę")
+      message.channel.send(`Pamiętaj, aby korzystać z bota, musisz posiadać rangę Swój Gość (info pod komendą !swojgosc)\nKomendy dla wszystkich:\n!help - wyświetla listę komend oraz ich przeznaczenie\n!facebook - wyświetla link do facebooka GWD\n!grupa - wyświetla link do grupy Astronomia-Polska\n!meteo - wyświetla link do grupy meteorologia-polska\n!stellarium - link do programu stellarium\n!swiatlo - link do lightpollutionmap\n!swojgosc(dostępna dla nieposiadających tej rangi) - informacje na temat rangi Swój Gość\n!randomuser - wybiera losowego użytkownika serwera\n!zjawisko - wyświetla dzisiejsze zjawisko astronomiczne\n!zjawisko [dzień/miesiac/rok] - wyświetla zjawisko na dany dzień\n!ranga [link do Twojego facebooka] - dostępna tylko na kanale ${kanal}, użyj jej jeśli chcesz uzyskać rangę Swój Gość\n\nKomendy dla administratorów:\n!clearchannel [liczba] - usuwa daną ilość wiadomości z kanału\n!ban [oznacz użytkownika] - chyba nie trzeba wyjaśniać\n!kick [oznacz użytkownka] - wyrzuca (nie banuje) danego użytkownika)
+      `)
+  }
+
+
+  if(command === "facebook")
+    {
+      message.channel.send("Facebook Gwiazdy w Dłoniach: https://www.facebook.com/gwiazdywdloniach");
+    }
+
+  if(command === "grupa")
+  {
+      message.channel.send("Wpadnij na grupę Astronomia-Polska: https://www.facebook.com/groups/astronomiapolska");
+  }
+
+  if(command === "meteo")
+  {
+      message.channel.send("Grupa Meteorologia-Polska: https://www.facebook.com/groups/meteopolska");
+  }
+
+  if(command === "stellarium")
+    {
+      message.channel.send("Interaktywna mapa nieba: https://stellarium.org");
+    }
+
+  if(command === "swiatlo")
+  {
+      message.channel.send("Mapa zanieczyszczenia światłem: https://www.lightpollutionmap.info/");
+  }
+
+  if(command === "randomuser")
+  {
+      const member = message.guild.members.random()
+      message.channel.send(`Wybieram ${member}!`);
+  }
+
+
+ if(command === "zjawisko" || command==="zjawiska")
+  {
+    let data = args[0]
+    if (!data)
+    {
+      const lista = require("./zjawiska.json");
+      let dzisiaj = new Date();
+      let dzien = dzisiaj.getDate();
+      let miesiac = dzisiaj.getMonth()+1;
+      let rok = dzisiaj.getFullYear();
+      dzisiaj = dzien + "/" + miesiac + "/" + rok;
+      let entry = lista.find(post => post.data === dzisiaj)
+      if(!entry) return message.channel.send("Brak zjawisk na dziś :(");
+      message.channel.send(`Dziś na niebie: ${entry.nazwa}`)
+     }
+  else
+  {
+       const plik = require("./zjawiska.json");
+       let entry = plik.find(post => post.data === data);
+       if(!entry) return message.channel.send("Brak zjawisk na ten dzień");
+       message.channel.send(`Dnia ${data} nastąpi: ${entry.nazwa}`)
+  }
+  }
+
+if(command === "elon")
+{
+  message.channel.send({
+      file: "https://i.imgur.com/O0JU8mH.jpg"
+  });
+}
+
+if(command === "ksiezyc")
+{
+  message.channel.send({
+    file: "https://i.imgur.com/jO5xbDr.jpg"
+  })
+}
+
+
+ }
+  else
+  {
+   return
+   message.reply("Tylko użytkownicy posiadający rangę Swój Gość mogą używać bota. Info pod komendą !swojgosc")
+  }
+
+
+
+//--------------------------dla adminów----------------------------------
+
+
+if(message.member.roles.has(admin.id))
+{
+  if(command==="clear")
+  {
+    let liczba = args [0]
+    if(!liczba) return message.reply("Podaj ilość wiadomości do usunięcia")
+    message.channel.bulkDelete(liczba)
+  }
+
   if(command==="ban")
   {
-    if(!message.member.roles.has(admin.id)) return message.reply("Ta komenda jest dostępna tylko dla administratorów")
-    let user = message.mentions.members.first()
-    user.ban
+   let user = message.mentions.users.first()
+   if (!user) return message.reply("Oznacz użytkownika, którego chcesz zbanować!")
+   let member = message.guild.member(user)
+   if(!member) return
+   member.ban
+   message.channel.send(`${member} został zbanowany, hańba Ci ${member}`)
   }
 
-  if(command === "kick")
+  if(!command==="kick")
   {
-    if(!message.member.roles.has(admin.id)) return message.reply("Ta komenda jest dostępna tylko dla administratorów")
-    let user = message.mentions.members.first()
-    user.kick
+  let user = message.mentions.users.first()
+  if (!user) return message.reply("Oznacz użytkownika, którego chcesz wyrzucić!")
+  let member = message.guild.member(user)
+  if(!member) return
+  member.kick
+  message.channel.send(`${member} został wyrzucony, hańba Ci ${member}`)
   }
+}
 
+else
+{
+  message.reply("Ta komenda jest dostępna tylko dla adminisratorów")
 }
 
 
@@ -194,6 +211,10 @@ if(message.content.startsWith(prefix))
 
 
 
+
+
+
+}
 
 
 
